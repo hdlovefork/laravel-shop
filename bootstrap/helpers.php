a8b8c17a2a0b345b03392b88b2b6f86ca97fb710
dean <1602264241@qq.com>
2018-11-27 14:24:50 +0800

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -11,3 +11,12 @@ if(!function_exists('test_helper')){
         return 'OK';
     }
 }
+
+if(!function_exists('route_class')){
+
+    function route_class()
+    {
+        return str_replace('.', '-', Route::currentRouteName());
+    }
+}
+
