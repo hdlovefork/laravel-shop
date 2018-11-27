bc1c64de8289a50215121791c6f7ef7dd4034276
dean <1602264241@qq.com>
2018-11-27 21:34:49 +0800

    异常处理

---
+++ app/Exceptions/InternalException.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Http\Request;
+use Throwable;
+
+class InternalException extends Exception
+{
+    /**
+     * @var string
+     */
+    private $msgForUser;
+
+    public function __construct(string $message = "", string $msgForUser='系统内部错误', int $code = 500, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->msgForUser = $msgForUser;
+    }
+
+    public function render(Request $request){
+        if($request->expectsJson()){
+            return response()->json(['msg'=>$this->msgForUser,'code'=>$this->code]);
+        }
+        return view('pages.error',['msg'=>$this->msgForUser]);
+    }
+}
