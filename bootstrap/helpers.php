5366b14c18a446f4ffcc7277b7decbd7e9378e51
dean <1602264241@qq.com>
2018-11-27 11:07:48 +0800

    辅助函数

---
+++ bootstrap/helpers.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dean
+ * Email: 1602264241@qq.com
+ * Date: 2018/11/27
+ * Time: 上午11:02
+ */
+if(!function_exists('test_helper')){
+    function test_helper(){
+        return 'OK';
+    }
+}
