9e8902bfdd93ce8edf71e17b29469c3e1efc8f39
dean <1602264241@qq.com>
2018-11-28 21:42:39 +0800

    管理后台 - 商品列表

--- app/Admin/Controllers/UsersController.php
+++ app/Admin/Controllers/UsersController.php
@@ -23,8 +23,8 @@ class UsersController extends Controller
     public function index(Content $content)
     {
         return $content
-            ->header('Index')
-            ->description('description')
+            ->header('用户列表')
+            ->description('用户列表')
             ->body($this->grid());
     }
 
