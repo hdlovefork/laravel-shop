215e4e2546fe7922d8be9c1a9c714b0c11ceaa36
dean <1602264241@qq.com>
2018-11-29 17:04:29 +0800

    用户界面 - 商品详情页

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -24,19 +24,21 @@
                     </div>
                     <div class="row products-list">
                         @foreach($products as $product)
-                            <div class="col-xs-3 product-item">
-                                <div class="product-content">
-                                    <div class="top">
-                                        <div class="img"><img src="{{ $product->image_url }}" alt=""></div>
-                                        <div class="price"><b>￥</b>{{ $product->price }}</div>
-                                        <div class="title">{{ $product->title }}</div>
-                                    </div>
-                                    <div class="bottom">
-                                        <div class="sold_count">销量 <span>{{ $product->sold_count }}笔</span></div>
-                                        <div class="review_count">评价 <span>{{ $product->review_count }}</span></div>
+                            <a href="{{ route('products.show',['$product'=>$product->id]) }}">
+                                <div class="col-xs-3 product-item">
+                                    <div class="product-content">
+                                        <div class="top">
+                                            <div class="img"><img src="{{ $product->image_url }}" alt=""></div>
+                                            <div class="price"><b>￥</b>{{ $product->price }}</div>
+                                            <div class="title">{{ $product->title }}</div>
+                                        </div>
+                                        <div class="bottom">
+                                            <div class="sold_count">销量 <span>{{ $product->sold_count }}笔</span></div>
+                                            <div class="review_count">评价 <span>{{ $product->review_count }}</span></div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                            </a>
                         @endforeach
                     </div>
                     <div class="pull-right">{{ $products->appends($filters)->render() }}</div>
