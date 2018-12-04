80cec1ad65cf095b39df87e1b0cbfc10f6716b77
dean <1602264241@qq.com>
2018-12-04 15:03:10 +0800

    优化优惠券模块

--- app/Http/Controllers/CouponCodesController.php
+++ app/Http/Controllers/CouponCodesController.php
@@ -3,19 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Exceptions\CouponCodeUnavailableException;
+use App\Http\Requests\Request;
 use App\Models\CouponCode;
-use Illuminate\Support\Carbon;
 
 class CouponCodesController extends Controller
 {
-    public function show($code)
+    public function show(Request $request,$code)
     {
         // 判断优惠券是否存在
         if (!$record = CouponCode::where('code', $code)->first()) {
             throw new CouponCodeUnavailableException('优惠券不存在');
         }
 
-        $record->checkAvailable();
+        $record->checkAvailable($request->user());
         return $record;
     }
 }
---
+++ database/seeds/CouponCodeSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CouponCodeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i=0;$i<10000;$i++){
+            factory(\App\Models\CouponCode::class,100)->create();
+            $this->command->info('已生成' . ($i+1) * 100 . '条记录');
+            sleep(10);
+        }
+    }
+}
